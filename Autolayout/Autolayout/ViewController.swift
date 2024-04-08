bb7c2e99266bc14a64eff13c00aea98cea1e251d
soeun11 <134983918+soeun11@users.noreply.github.com>
2024-04-08 18:56:57 +0900

    Add files via upload

---
+++ Autolayout/Autolayout/ViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var foodlayer: UIView!
+    
+    @IBOutlet weak var cuponlayer: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+        
+        
+        
+    }
+    
+    
+    
+}
